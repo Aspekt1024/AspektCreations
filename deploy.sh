9aa8ddb9f38f1cc9c6916383d6e388e2883accda
DESKTOP-2N5MTEF\Dan <d.szabo87@gmail.com>
2017-02-04 11:43:13 +1100

    trying out what the guy said...

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+GAE_PROJECT=aspektcreations
+DEPLOY_VERSION=$1
+
+if [ -z "$DEPLOY_VERSION" ]
+then
+  TAG=`git describe --abbrev=0`
+  # GAE doesn't allow periods
+  DEPLOY_VERSION=${TAG//.}
+fi
+
+# Build it.
+echo "Building $DEPLOY_VERSION"
+gulp
+cp app.yaml dist/app.yaml
+
+echo "Deploying $DEPLOY_VERSION"
+gcloud preview app deploy dist/app.yaml --project $GAE_PROJECT --promote --version $DEPLOY_VERSION
